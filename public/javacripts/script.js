6d7b2dca4c33340ec4ea0ee9893bb3dbec5adbe9
itsdroid <shubhampopalghat77@gmail.com>
2026-01-02 23:48:50 +0530

    updated

--- public/javacripts/script.js
+++ public/javacripts/script.js
@@ -1,41 +1,41 @@
-// to toglle the password hide unhide
+// to toggle the password hide unhide
 function togglePassword() {
     const passwordInput = document.getElementById('password');
     const toggleIcon = document.getElementById('toggleIcon');
 
     if (passwordInput.type === 'password') {
         passwordInput.type = 'text';
-        toggleIcon.src = '/images/eye.svg';  
+        toggleIcon.src = '/images/eye.svg';
     } else {
         passwordInput.type = 'password';
-        toggleIcon.src = '/images/eye-off.svg';  
+        toggleIcon.src = '/images/eye-off.svg';
     }
 }
 
 
 //flash message fade animation
 window.addEventListener('DOMContentLoaded', () => {
-const flashMessages = document.querySelectorAll('.flash-message');
+    const flashMessages = document.querySelectorAll('.flash-message');
 
-flashMessages.forEach(msg => {
-    setTimeout(() => {
-        msg.classList.add('opacity-0', 'transition-opacity', 'duration-500');  // Add fade classes
-
+    flashMessages.forEach(msg => {
         setTimeout(() => {
-            msg.remove();  
-        }, 500);  
-    }, 1000); 
-});
+            msg.classList.add('opacity-0', 'transition-opacity', 'duration-500');  // Add fade classes
 
-// Date and time update
-if (document.getElementById('datetime')) {
-    function updateDateTime() {
-        const now = new Date();
-        document.getElementById('datetime').innerText = now.toLocaleString();
+            setTimeout(() => {
+                msg.remove();
+            }, 500);
+        }, 1000);
+    });
+
+
+    if (document.getElementById('datetime')) {
+        function updateDateTime() {
+            const now = new Date();
+            document.getElementById('datetime').innerText = now.toLocaleString();
+        }
+        updateDateTime();
+        setInterval(updateDateTime, 1000);
     }
-    updateDateTime();
-    setInterval(updateDateTime, 1000);
-}
 });
 
 
@@ -48,14 +48,13 @@ async function validateEmail() {
     const email = document.getElementById("email").value;
     const apiKey = "a84c7fb312224cea9a63469748e80a43"; // Replace with actual API key
     const url = `https://api.hunter.io/v2/email-verifier?email=${email}&api_key=${apiKey}`;
-  
+
     const response = await fetch(url);
     const data = await response.json();
-  
+
     if (data.data.status === "valid") {
-      req.flash('success', 'valid email');
+        req.flash('success', 'valid email');
     } else {
-      req.flash('error' , 'invalid email');
+        req.flash('error', 'invalid email');
     }
-  }
-  
+}
